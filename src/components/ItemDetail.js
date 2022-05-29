83de8c6265094cffbc98d0bbf6cdc12d8eeb4f9a
Dungeon <sebastianmolinarojas@gmail.com>
2022-05-28 22:07:47 -0400

    ItemDetail hardcode

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,13 +1,14 @@
 import Card from 'react-bootstrap/Card';
 
 const ItemDetail = ({item}) => {
-  const {pictureUrl, title, description} = item
+  const {pictureUrl, title, price, description} = item
   return(
     <div>
+      <Card.Header bsPrefix="cardTitle">{title}</Card.Header>
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={pictureUrl} />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title>{`$${price}`}</Card.Title>
           <Card.Text>{description}</Card.Text>
         </Card.Body>
       </Card>
